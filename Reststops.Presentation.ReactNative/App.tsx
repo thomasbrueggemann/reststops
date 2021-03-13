1975b009e1c526d5e155dac5efa8e47964720d6e
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-03-13 22:44:39 +0100

    Closes #4, I18n setup complete

--- Reststops.Presentation.ReactNative/App.tsx
+++ Reststops.Presentation.ReactNative/App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { StatusBar } from "react-native";
+import React, { useEffect } from "react";
+import { NativeModules, Platform, StatusBar } from "react-native";
 import DestinationContext from "./contexts/DestinationContext";
 import Destination from "./screens/Destination";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,8 +10,21 @@ import "./i18n";
 
 const Stack = createStackNavigator();
 
+const deviceLanguage =
+	Platform.OS === "ios"
+		? NativeModules.SettingsManager.settings.AppleLocale ||
+		  NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
+		: NativeModules.I18nManager.localeIdentifier;
+
 const App = () => {
 	const [t, i18n] = useTranslation();
+
+	useEffect(() => {
+		const locale = deviceLanguage.split("_")[0].split("-")[0];
+		console.log("device language", deviceLanguage, "set locale to", locale);
+
+		i18n.changeLanguage(locale);
+	}, [deviceLanguage]);
 
 	return (
 		<DestinationContext.ContextProvider>
@@ -21,7 +34,7 @@ const App = () => {
 					<Stack.Screen
 						name="Destination"
 						component={Destination}
-						options={{ title: t("setYourDestination") }}
+						options={{ title: t("destination") }}
 					/>
 					<Stack.Screen name="Map" component={Map} options={{ title: t("reststops") }} />
 				</Stack.Navigator>
