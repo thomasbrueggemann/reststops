492190df20d812d1eb823e015b8ade1e7a36c912
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-11-27 21:38:49 +0100

    use distance table

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -6,10 +6,9 @@ use ::reststops::osrm::Osrm;
 use ::reststops::reststop::{Reststop, ReststopCategory};
 use futures::future::join_all;
 use futures::stream::TryStreamExt;
-use futures::Future;
 use geo_types::{Coordinate, LineString};
 use mongodb::bson::doc;
-use mongodb::options::ClientOptions;
+use mongodb::options::{ClientOptions, FindOptions};
 use mongodb::{Client, Collection};
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
@@ -29,11 +28,11 @@ struct ReststopResponse {
     pub category: ReststopCategory,
     pub location: Vec<f64>,
     pub tags: HashMap<String, String>,
-    pub detour_seconds: u32,
+    pub detour_seconds: f64,
 }
 
 impl ReststopResponse {
-    pub fn from(reststop: &Reststop, detour_seconds: u32) -> ReststopResponse {
+    pub fn from(reststop: &Reststop, detour_seconds: f64) -> ReststopResponse {
         ReststopResponse {
             name: reststop.name.to_owned(),
             description: reststop.description.to_owned(),
@@ -56,7 +55,7 @@ async fn reststops(
     start_lat: f64,
     end_lon: f64,
     end_lat: f64,
-    max_detour_seconds: i32,
+    max_detour_seconds: f64,
 ) -> Json<Vec<ReststopResponse>> {
     let start = Coordinate {
         x: start_lon,
@@ -71,23 +70,52 @@ async fn reststops(
     let route = Osrm::route(vec![start, end]).await.unwrap();
     let reststops = get_reststops(route).await;
 
+    let durations = get_duration_table(start, end, &reststops).await;
+    let responses = assign_durations_to_reststops(&durations, &reststops);
+
     println!("{} reststops found", reststops.len());
 
-    let response_reststops = reststops
+    Json(responses)
+}
+
+fn assign_durations_to_reststops(
+    durations: &Vec<Vec<f64>>,
+    reststops: &Vec<Reststop>,
+) -> Vec<ReststopResponse> {
+    let total_duration = durations[0][reststops.len() - 1];
+
+    let mut response_reststops: Vec<ReststopResponse> = vec![];
+
+    for i in 0..reststops.len() - 1 {
+        let duration_to_reststop = durations[0][i + 1];
+        let duration_from_reststop = durations[i + 1][reststops.len() - 1];
+
+        let detour_seconds = (duration_to_reststop + duration_from_reststop) - total_duration;
+        let response_reststop = ReststopResponse::from(&reststops[i], detour_seconds);
+
+        response_reststops.push(response_reststop);
+    }
+
+    return response_reststops;
+}
+
+async fn get_duration_table(
+    start: Coordinate<f64>,
+    end: Coordinate<f64>,
+    reststops: &Vec<Reststop>,
+) -> Vec<Vec<f64>> {
+    let mut distance_coords: Vec<Coordinate<f64>> = vec![start];
+    let mut reststop_coords: Vec<Coordinate<f64>> = reststops
         .iter()
-        .map(|reststop| async {
-            let detour = Osrm::route(vec![start, reststop.to_coordinate(), end])
-                .await
-                .unwrap();
+        .map(|reststop| reststop.to_coordinate())
+        .collect();
 
-            ReststopResponse::from(reststop, 0)
-        })
-        .collect::<Vec<Future<ReststopResponse>>>();
+    distance_coords.append(&mut reststop_coords);
+    distance_coords.push(end);
 
-    let joined = join_all(response_reststops).await;
-    // TODO: enrich with detours
+    let table = Osrm::table(distance_coords).await.unwrap();
 
-    Json(response_reststops)
+    return table;
 }
 
 async fn get_reststops(route: LineString<f64>) -> Vec<Reststop> {
@@ -110,9 +138,11 @@ async fn get_reststops(route: LineString<f64>) -> Vec<Reststop> {
         }
     };
 
+    let find_options = FindOptions::builder().limit(100).build();
+
     let reststops_col = get_reststops_collection().await;
     let result: Vec<Reststop> = reststops_col
-        .find(filter, None)
+        .find(filter, find_options)
         .await
         .unwrap()
         .try_collect()
--- src/osrm.rs
+++ src/osrm.rs
@@ -37,21 +37,63 @@ pub struct Leg {
     pub duration: f64,
 }
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct TableResult {
+    pub code: String,
+    pub durations: Vec<Vec<f64>>,
+    pub sources: Vec<Source>,
+    pub destinations: Vec<Destination>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Source {
+    pub hint: String,
+    pub distance: f64,
+    pub location: Vec<f64>,
+    pub name: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Destination {
+    pub hint: String,
+    pub distance: f64,
+    pub location: Vec<f64>,
+    pub name: String,
+}
+
 pub struct Osrm {}
 
 impl Osrm {
     pub async fn route(coordinates: Vec<Coordinate<f64>>) -> Result<LineString<f64>> {
-        let base_url = "https://routing.openstreetmap.de/routed-car/route/v1/driving/";
+        let url = Osrm::build_url(coordinates, "route");
+        let route_result = reqwest::get(url).await?.json::<RouteResult>().await?;
+
+        let polyline = polyline::decode_polyline(&route_result.routes[0].geometry, 5).unwrap();
+
+        Ok(polyline)
+    }
+
+    pub async fn table(coordinates: Vec<Coordinate<f64>>) -> Result<Vec<Vec<f64>>> {
+        let url = Osrm::build_url(coordinates, "table");
+        let table_result = reqwest::get(url).await?.json::<TableResult>().await?;
+
+        Ok(table_result.durations)
+    }
+
+    fn build_url(coordinates: Vec<Coordinate<f64>>, service: &str) -> String {
+        let base_url = "https://routing.openstreetmap.de/routed-car";
 
         let url_coordinate_strings: Vec<String> = coordinates
             .iter()
             .map(|coord| format!("{},{}", coord.x, coord.y))
             .collect();
 
-        let url = format!("{}{}", base_url, url_coordinate_strings.join(";"));
-        let route_result = reqwest::get(url).await?.json::<RouteResult>().await?;
-        let polyline = polyline::decode_polyline(&route_result.routes[0].geometry, 5).unwrap();
-
-        Ok(polyline)
+        let url = format!(
+            "{}/{}/v1/driving/{}",
+            base_url,
+            service,
+            url_coordinate_strings.join(";")
+        );
+        return url;
     }
 }
